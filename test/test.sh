ce7b1c2e7d8ddb819a5741150da308f29ffa9ad1
Krystof Liskovec <krystof.liskovec@gmail.com>
2023-01-01 18:04:52 +0100

    Fix test

--- test/test.sh
+++ test/test.sh
@@ -6,9 +6,9 @@ echo "Strategy: $strategy"
 
 $dir/../build/find-willa -s $1 $dir
 
-find $dir/find-willa -iname Willa
-find $dir/find-willa -iname alliW
-find $dir/find-willa ! -name ".willa-config" -exec grep -H -i "Willa" {} \; 2> /dev/null
-find $dir/find-willa -perm 020
+find $dir/find-willa-game -iname Willa
+find $dir/find-willa-game -iname alliW
+find $dir/find-willa-game ! -name ".willa-config" -exec grep -H -i "Willa" {} \; 2> /dev/null
+find $dir/find-willa-game -perm 020
 
-rm -rf $dir/find-willa
+rm -rf $dir/find-willa-game
